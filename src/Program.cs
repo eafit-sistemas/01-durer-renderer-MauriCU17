b0a73abd6848333fed902043088963b0f9cb3901
mauriciou1 <mauricio.u@yumebau.com>
2026-02-01 16:10:30 -0500

    implementation of durer rendering algorith

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,9 @@
 using System;
+using SkiaSharp;
+using System.IO;
 
 public static class Program
-{
-    public static void Main()
+{public static void Main()
     {
         InputData data = InputData.LoadFromJson("input.json");
         Shape2D projected = ProjectShape(data.Model);
@@ -12,15 +13,73 @@ public static class Program
 
     private static void Render(Shape2D shape, RenderParameters parameters, string outputPath)
     {
-        //TODO: Use skiasharp to render the shape to the output path, the test checks for this location
-        // with the given parameters.
-        throw new NotImplementedException();
+        string projectRoot = Path.GetFullPath(
+        Path.Combine(AppContext.BaseDirectory, "..", "..", "..")
+    );
+
+    string finalPath = Path.Combine(projectRoot, outputPath);
+
+    int res = parameters.Resolution;
+
+    using var bitmap = new SKBitmap(res, res);
+    using var canvas = new SKCanvas(bitmap);
+
+    canvas.Clear(SKColors.White);
+
+    using var paint = new SKPaint
+    {
+        Color = SKColors.Black,
+        StrokeWidth = 2,
+        IsAntialias = true
+    };
+
+    SKPoint ToScreen(float x, float y)
+    {
+        float px = (x - parameters.XMin) / (parameters.XMax - parameters.XMin) * res;
+        float py = res - ((y - parameters.YMin) / (parameters.YMax - parameters.YMin) * res);
+        return new SKPoint(px, py);
+    }
+
+    foreach (var line in shape.Lines)
+    {
+        var p0 = shape.Points[line[0]];
+        var p1 = shape.Points[line[1]];
+
+        SKPoint a = ToScreen(p0[0], p0[1]);
+        SKPoint b = ToScreen(p1[0], p1[1]);
+
+        canvas.DrawLine(a, b, paint);
+    }
+
+    using var image = SKImage.FromBitmap(bitmap);
+    using var data = image.Encode(SKEncodedImageFormat.Jpeg, 100);
+
+    File.WriteAllBytes(finalPath, data.ToArray());
     }
 
     private static Shape2D ProjectShape(Model3D model)
     {
-        //TODO: Implement projection logic
-        throw new NotImplementedException();
+        int vertexCount = model.VertexTable.Length;
+
+    float[][] points2D = new float[vertexCount][];
+
+    for (int i = 0; i < vertexCount; i++)
+    {
+        float x = model.VertexTable[i][0];
+        float y = model.VertexTable[i][1];
+        float z = model.VertexTable[i][2];
+
+        float px = x / z;
+        float py = y / z;
+
+        points2D[i] = new float[] { px, py };
+    }
+
+    return new Shape2D
+    {
+        Points = points2D,
+        Lines = model.EdgeTable
+    };
     }
 }
 
